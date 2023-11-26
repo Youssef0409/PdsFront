69f50df99d5d5156228b47987ebc9cbdcbce8b70
youssef.mazlout <youssef.mazlout@sesame.com.tn>
2023-11-26 15:59:08 +0100

    1

---
+++ src/app/find-job/find-job.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-job',
+  templateUrl: './find-job.component.html',
+  styleUrls: ['./find-job.component.scss']
+})
+export class FindJobComponent {
+
+}
---
+++ src/app/job-details/job-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-details',
+  templateUrl: './job-details.component.html',
+  styleUrls: ['./job-details.component.scss']
+})
+export class JobDetailsComponent {
+
+}
