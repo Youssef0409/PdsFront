60ae2bb2b973e99b20a5f798ed74eab637fd5d51
youssef.mazlout <youssef.mazlout@sesame.com.tn>
2023-11-26 15:52:41 +0100

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FindJobComponent } from './find-job/find-job.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: AccueilComponent },
+  { path: 'navbar', component: NavbarComponent },
+  { path: 'findJob', component: FindJobComponent },
+  { path: 'jobDetails', component: JobDetailsComponent },
+ 
+  // Add more routes as needed
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route
+  { path: '**', redirectTo: '/home' }, // Wildcard route for 404
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FindJobComponent } from './find-job/find-job.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    NavbarComponent,
+    FindJobComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule,
